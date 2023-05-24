fc1506092b876a1acd2d1d85241316655591440a
aymanelyamoun <ael-yamo@student.1337.ma>
2023-05-24 22:29:37 +0100

    cookies setup

--- inc/req_res.hpp
+++ inc/req_res.hpp
@@ -48,6 +48,7 @@ struct HTTP_request
 	string connection;
 	string body;
 	string query_string;
+	string cookies;
 	//construsctor for request empty strings and -1 in numbers
 	HTTP_request() : method(-1), requested_file(""), host(""), content_type(std::make_pair("", "")), content_length(-1), connection(""), body("") {}
 	// multimap<float, string> accept;
@@ -66,6 +67,7 @@ struct HTTP_response
 	bool close_connection;
 
 	string body;
+	string cookies;
 
 	bool stop;
 
