f9fdd5887e44e265e8ccb480e5d2d3e5f9a8acfa
Alaaeddine El korchi <elkorchi.alae@gmail.com>
2023-05-27 23:39:51 +0100

    fixing if timeout occured in child proccess and removed newline from post body in cgi

--- CGI/cgi-bin.py
+++ CGI/cgi-bin.py
@@ -1,13 +1,16 @@
 import sys
 
+
+# while 1:
+#     print("While 1 Example")
+
 # Check if an argument was provided
 if len(sys.argv) < 2:
     print("No input found !")
     sys.exit(1)
 
-# Get the first argument and remove newlines
 argument = sys.argv[1]
 length = len(argument)
 
 # Print the length of the argument
-print("Length of the argument:", length)
+print("Length of the argument:", length,"")
--- src/cgi/cgi.cpp
+++ src/cgi/cgi.cpp
@@ -1,17 +1,32 @@
 #include "../../inc/req_res.hpp"
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 
+pid_t childpid;
 
+void handleTimeout(int signum)
+{
+	(void)signum;
+	kill(childpid, SIGKILL);
+}
 
 int cgi(_cgi_info cgi_info , HTTP_request &request_info, HTTP_response &response)
 {
 	// time out 
+	//std::cout << "'" << request_info.body << "'\n";
 	const char* lang_path = cgi_info.lang_path.c_str();
 	const char* cgi_name = cgi_info.cgi_name.c_str();
 	std::string subtracted_body;
 	if (request_info.method == GET)
 		subtracted_body = request_info.query_string;
 	else
+	{
 		subtracted_body = request_info.body;
+		subtracted_body.erase(subtracted_body.size() - 1, 1);
+	}
 	subtracted_body.erase(0, 5);
 	const char* body = subtracted_body.c_str();
 	char *av[] = {(char *) lang_path, (char *) cgi_name, (char *) body, nullptr};
@@ -41,7 +56,21 @@ int cgi(_cgi_info cgi_info , HTTP_request &request_info, HTTP_response &response
 		close(fd);
 		exit(500);
 	}
-	wait(NULL);
+	childpid = pid;
+	signal(SIGALRM, handleTimeout);
+    alarm(5);
+	int status;
+    waitpid(pid, &status, 0);
+	alarm(0);
+	int signalNumber = WTERMSIG(status);
+	if (signalNumber > 0)
+	{
+		response.body = "script took too long to execute.";
+		response.content_length = response.body.size();
+		response.set_status(200, "OK");
+		response.content_type = "text/html";
+		return 1;
+	}
 	long fileSize = ftell(fp);
 	response.content_length = fileSize;
 	response.set_status(200, "OK");
